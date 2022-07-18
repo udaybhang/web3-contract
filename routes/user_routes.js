d3e7a227c59df8475f6a1eba920bca18cbb9f94d
udaybhang <46225651+udaybhang@users.noreply.github.com>
2022-07-18 10:17:55 +0530

    new changes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,11 +1,14 @@
 const accountController = require('../controllers/userController')
 const auth = require('../utils/auth')
+var baseCommonApiUrl = C.baseApiUrl;
 module.exports = function (app){
     // app.post('/signup',accountController.signup);
     // app.post('/login',accountController.login);
-    app.post('/v1/createAccount',accountController.createAccount);
-    app.post('/v1/createWallet',accountController.createWallet);
-    app.post('/v1/compileGenerateAbiByteCode',accountController.generateAbiByteCode);
-    app.post('/v1/deployContract',accountController.deployContract);
-    app.post('/v1/signTransaction',accountController.signTransaction);
+    app.post(`${baseCommonApiUrl}createAccount`,accountController.createAccount);
+    app.post(`${baseCommonApiUrl}createWallet`,accountController.createWallet);
+    app.post(`${baseCommonApiUrl}compileGenerateAbiByteCode`,accountController.generateAbiByteCode);
+    app.post(`${baseCommonApiUrl}deployContract`,accountController.deployContract);
+    app.post(`${baseCommonApiUrl}signTransaction`,accountController.signTransaction);
+    app.post(`${baseCommonApiUrl}getTransaction`,accountController.getTransaction);
+    app.post(`${baseCommonApiUrl}connectContract`,accountController.connectContract);
  };
